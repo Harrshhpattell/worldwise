da52749ce3480e7b1ff6effb53af1223d21d4488
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-10-13 21:44:56 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
-        <Route path="homepage" element={<Homepage />}></Route>
+        <Route path="app" element={<AppLayout />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
     </BrowserRouter>
   );
